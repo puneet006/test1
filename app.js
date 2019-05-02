0fe7adc1e4853935893ec758ab0f8727b9792f58
puneet <puneet006@gmail.com>
2019-05-02 18:18:32 +0530

    js update

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log("hrllo");
 console.log("hrllo");
+
+
+var a = prompt('enter first no.');
+var b = prompt('enter second no.');
+var c = parseInt(a) + parseInt(b); 
+
+
+console.log(c);     
